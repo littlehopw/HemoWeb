6b921244f1474f46d0ed2de0617a04161bb645b6
Maria Luísa Borges Martins <maria.lm@estudante.iftm.edu.br>
2025-04-01 21:00:49 -0300

    Feat: Landing Page #HEW-29
     Agora não quebra em Notebooks.

--- frontend/src/pages/PaginaInicial/hero.jsx
+++ frontend/src/pages/PaginaInicial/hero.jsx
@@ -2,9 +2,8 @@ import { Link } from 'react-router-dom';
 import vectorHero from '../../assets/vector-hero.png';
 
 function Hero() {
-  //COMENTÁRIO PARA NOVO COMMIT
   return (
-    <div className="max-w-full md:max-w-[110%] lg:mx-10 mx-0 md:mx-auto bg-[var(--primary-color-1)] rounded-none md:rounded-[40px] flex flex-col md:flex-row justify-between pt-24 px-6 md:px-[10%]">
+    <div className="max-w-full md:max-w-[110%] lg:mx-10 mx-0 md:mx-auto bg-[var(--primary-color-1)] rounded-none md:rounded-[40px] flex flex-col md:flex-row justify-between pt-24 px-6 md:px-[10%] min-h-[500px] md:min-h-[550px] lg:min-h-[600px]">
       <div className="flex flex-col justify-center">
         <h1 className="font-bold text-[65px] text-[var(--primary-color-4)]" style={{ fontFamily: 'var(--h2-poppins)' }}>
           Bem-vindo ao HemoWeb
@@ -24,11 +23,11 @@ function Hero() {
           </button>
         </Link>
       </div>
-      <div className="flex justify-center items-center mt-10 md:mt-0">
+      <div className="flex justify-center items-end mt-10 md:mt-0">
         <img
           src={vectorHero}
           alt="HemoWeb Hero"
-          className="object-contain md:object-contain h-[108%] w-[108%] md:h-[121.68%] md:w-[121.68%] lg:h-[591.5px] lg:w-[591.5px] self-center mb-0 transition-transform duration-300 ease-in-out hover:scale-105"
+          className="object-contain h-auto w-auto max-h-[100%] max-w-[100%] md:max-h-[550px] md:max-w-[550px] lg:max-h-[600px] lg:max-w-[600px] self-end mb-0 transition-transform duration-300 ease-in-out hover:scale-105"
         />
       </div>
     </div>
