5a3e1828799bcb93084fd67cd4b8e291a231bec8
Maria Luísa Borges <maria.lm@estudante.iftm.edu.br>
2025-05-04 19:23:00 -0300

    Mais um teste
    Segundo commit, só para enviar a versão base "certa"

--- frontend/src/pages/Perfil/notificacao.jsx
+++ frontend/src/pages/Perfil/notificacao.jsx
@@ -1,16 +1,23 @@
 import '../../App.css';
-import hemowebLogo from '../../assets/hemoweb-logo.png';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; // Certifique-se de que o Link está importado corretamente
 
-function Perfil() {
-    return (
-        <div className="flex">
-            <Sidebar />
-            <div className="bg-white min-h-screen flex-1 flex flex-col">
-               <p>testestestes</p>
-            </div>
-        </div>
-    );
+function Notificacao() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[var(--light-background-color)]">
+      <h1 className="text-[var(--primary-color-4)] text-[32px] font-bold mb-4">
+        Página de Notificações
+      </h1>
+      <p className="text-[var(--black)] text-[16px]">
+        Esta é a página de notificações.
+      </p>
+      <Link
+        to="/perfil"
+        className="mt-4 px-4 py-2 bg-[var(--primary-color-2)] text-[var(--white)] rounded-lg hover:bg-[var(--primary-color-2-hover)] transition-colors duration-300"
+      >
+        Voltar para o Perfil
+      </Link>
+    </div>
+  );
 }
 
 export default Notificacao;
